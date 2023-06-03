98bf00706d03b21724f8f9239b46abd84d7acd02
gimmy5512 <gimmy5512@gmail.com>
2023-06-03 11:35:11 +0800

    python 6/3

--- lesson9_1.py
+++ lesson9_1.py
@@ -1,2 +1,16 @@
 import streamlit as st
-st.write("#hello world")
+
+"#### Hello world Robert"
+"Hello ***徐國堂***"
+
+st.markdown("Hello! **world**")
+st.divider()
+st.title("這是app的title")
+st.divider()
+st.header("這是header")
+st.divider()
+st.subheader("這是subheader")
+st.divider()
+st.caption("這是caption")
+st.divider
+st.code("a=123")
