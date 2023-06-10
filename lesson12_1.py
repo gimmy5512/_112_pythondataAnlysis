7df45fb38c58fd0290031ed450fecd0f60dee466
gimmy5512 <49330508+gimmy5512@users.noreply.github.com>
2023-06-10 07:52:59 +0000

    stock

---
+++ lesson12_1.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+
+codeFrame = pd.read_csv('codeSearch.csv',usecols=['code','name'])
+codeSeries = codeFrame['code'].astype(str) + codeFrame['name']
+
+with st.sidebar:
+    selected_codes = st.multiselect("請選擇股票:",codeSeries,
+                                    max_selections=4)
+    
+@st.cache_data
+def fetch_stock_dataFrame(id):
+    stock_dataFrame = yf.download(id,start='2022-01-01')
+    return stock_dataFrame
+
+for code in selected_codes:
+    code1 = code[:4]+'.TW'
+    code_stock_dataFrame = fetch_stock_dataFrame(code1)
+    code_stock_dataFrame_sorted = code_stock_dataFrame.sort_index(ascending=False)
+    st.subheader(code)
+    st.dataframe(code_stock_dataFrame_sorted,width=1024)
+    st.divider()
+
+
